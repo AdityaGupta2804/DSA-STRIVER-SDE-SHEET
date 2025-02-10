7025e2e45952fa7c172b9faf18ed87db4bc7e3c6
AdityaGupta2804 <codewithadi28@gmail.com>
2025-02-10 20:10:49 +0530

    MergingtwoSortedArray

---
+++ MergeTwoSortedArray.cpp
@@ -0,0 +1,90 @@
+/* 
+    author: Aditya Gupta 
+    created: 10.02.2025 19:44:13
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifndef ONLINE_JUDGE
+// Template to debug 1 variable
+template<typename T>
+void debug1(const char* varName, T varValue) {
+    cout << varName << " = " << varValue << endl;
+}
+// Template to debug 2 variables
+template<typename T1, typename T2>
+void debug2(const char* varName1, T1 varValue1, const char* varName2, T2 varValue2) {
+    cout << varName1 << " = " << varValue1 << " " << varName2 << " = " << varValue2 << endl;
+}
+// Template to print a vector
+template<typename T>
+void debug_vector(const char* varName, const vector<T>& vec) {
+    cout << varName << " = [";
+    for (size_t i = 0; i < vec.size(); ++i) {
+        cout << vec[i];
+        if (i < vec.size() - 1) cout << ", ";
+    }
+    cout << "]" << endl;
+}
+
+#define debug1(var) debug1(#var, var)
+#define debug2(var1, var2) debug2(#var1, var1, #var2, var2)
+#define debug_vector(vec) debug_vector(#vec, vec)
+
+#else
+#define debug1(var)
+#define debug2(var1, var2)
+#define debug_vector(vec)
+#endif
+
+#define int long long
+void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+    m--,n--;
+    int last = m+n+1;
+    while(m>=0 && n>=0){
+        if(nums1[m]>=nums2[n]){
+            nums1[last--] = nums1[m--];
+        }else{
+            nums1[last--] = nums2[n--]; 
+        }
+    }
+    while(n>=0){
+        nums1[last--] = nums2[n--];
+    }
+}
+void solve(){
+   int n,m;
+   cin>>n>>m;
+   vector<int> nums1(n+m,0),nums2(n);
+   for(int i=0;i<m;i++){
+    cin>>nums1[i];
+   }
+   for(int i=0;i<n;i++){
+    cin>>nums2[i];
+   }
+   merge(nums1,m,nums2,n);
+   for(auto it: nums1){
+    cout<<it<<" ";
+   }
+   cout<<endl;
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    auto start = chrono::high_resolution_clock::now();
+
+    int t;
+    cin >> t;
+    
+    while (t--) {
+        solve();
+    }
+
+    auto end = chrono::high_resolution_clock::now();
+    auto duration = chrono::duration_cast<chrono::milliseconds>(end - start);
+    cerr << "Execution Time: " << duration.count() << " ms" << std::endl;
+
+    return 0;
+}
