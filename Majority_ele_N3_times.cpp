0a60fcd66f7a3555091e59e67625a4bc6b2a2561
AdityaGupta2804 <codewithadi28@gmail.com>
2025-02-18 18:57:27 +0530

    BooyeMoorealgorithm with 2 candadtes , and 2 count variable

---
+++ Majority_ele_N3_times.cpp
@@ -0,0 +1,107 @@
+/* 
+    author: Aditya Gupta 
+    created: 18.02.2025 18:26:10
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifndef ONLINE_JUDGE
+// Template to debug 1 variable
+template<typename T>
+void debug1(const char* varName, T varValue) {
+    cout << varName << " = " << varValue << endl;
+}
+// Template to debug 2 variables
+template<typename T1, typename T2>
+void debug2(const char* varName1, T1 varValue1, const char* varName2, T2 varValue2) {
+    cout << varName1 << " = " << varValue1 << " " << varName2 << " = " << varValue2 << endl;
+}
+// Template to print a vector
+template<typename T>
+void debug_vector(const char* varName, const vector<T>& vec) {
+    cout << varName << " = [";
+    for (size_t i = 0; i < vec.size(); ++i) {
+        cout << vec[i];
+        if (i < vec.size() - 1) cout << ", ";
+    }
+    cout << "]" << endl;
+}
+
+#define debug1(var) debug1(#var, var)
+#define debug2(var1, var2) debug2(#var1, var1, #var2, var2)
+#define debug_vector(vec) debug_vector(#vec, vec)
+
+#else
+#define debug1(var)
+#define debug2(var1, var2)
+#define debug_vector(vec)
+#endif
+
+#define int long long
+vector<int> majorityElement(vector<int>& nums) {
+    int n = nums.size();
+    if (n == 0) return {};  // Edge case: empty array
+    
+    // Step 1: Find potential candidates using Boyer-Moore Voting Algorithm
+    int candidate1 = 0, candidate2 = 0, count1 = 0, count2 = 0;
+    
+    for (int num : nums) {
+        if (num == candidate1) {
+            count1++;
+        } else if (num == candidate2) {
+            count2++;
+        } else if (count1 == 0) {
+            candidate1 = num;
+            count1 = 1;
+        } else if (count2 == 0) {
+            candidate2 = num;
+            count2 = 1;
+        } else {
+            count1--;
+            count2--;
+        }
+    }
+
+    count1 = count2 = 0;
+    for (int num : nums) {
+        if (num == candidate1) count1++;
+        else if (num == candidate2) count2++;
+    }
+
+    vector<int> result;
+    if (count1 > n / 3) result.push_back(candidate1);
+    if (count2 > n / 3) result.push_back(candidate2);
+
+    return result;
+}
+void solve(){
+    int n;
+    cin>>n;
+    vector<int> nums(n);
+    for(auto &it : nums){
+         cin>>it;
+    }
+    vector<int> ans = majorityElement(nums);
+    cout<<ans[0]<<" "<<ans[1]<<endl;
+}
+
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    auto start = chrono::high_resolution_clock::now();
+
+    int t;
+    cin >> t;
+    
+    while (t--) {
+        solve();
+    }
+
+    auto end = chrono::high_resolution_clock::now();
+    auto duration = chrono::duration_cast<chrono::milliseconds>(end - start);
+    cerr << "Execution Time: " << duration.count() << " ms" << std::endl;
+
+    return 0;
+}
