a6eef64c8c2ad1dd1eaccb60e6e2c33f8785ab3c
AdityaGupta2804 <codewithadi28@gmail.com>
2025-02-21 17:06:00 +0530

    foursome

---
+++ 4Sum.cpp
@@ -0,0 +1,110 @@
+/* 
+    author: Aditya Gupta 
+    created: 21.02.2025 14:54:35
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifndef ONLINE_JUDGE
+// Template to debug 1 variable
+template<typename T>
+void debug1(const char* varName, T varValue) {
+    cout << varName << " = " << varValue << endl;
+}
+// Template to debug 2 variables
+template<typename T1, typename T2>
+void debug2(const char* varName1, T1 varValue1, const char* varName2, T2 varValue2) {
+    cout << varName1 << " = " << varValue1 << " " << varName2 << " = " << varValue2 << endl;
+}
+// Template to print a vector
+template<typename T>
+void debug_vector(const char* varName, const vector<T>& vec) {
+    cout << varName << " = [";
+    for (size_t i = 0; i < vec.size(); ++i) {
+        cout << vec[i];
+        if (i < vec.size() - 1) cout << ", ";
+    }
+    cout << "]" << endl;
+}
+
+#define debug1(var) debug1(#var, var)
+#define debug2(var1, var2) debug2(#var1, var1, #var2, var2)
+#define debug_vector(vec) debug_vector(#vec, vec)
+
+#else
+#define debug1(var)
+#define debug2(var1, var2)
+#define debug_vector(vec)
+#endif
+
+#define int long long
+vector<vector<int>> fourSum(vector<int>& nums, int target) {
+    int n = nums.size();
+    vector<vector<int>> ans;
+    sort(nums.begin(),nums.end());
+    for(int i=0;i<n;i++){
+        if(i>0  && nums[i] == nums[i-1]){
+            continue;
+        }
+        for(int j = i+1; j<n;j++){
+            if(j>i+1  && nums[j] == nums[j-1]){
+                continue;
+            }
+            int k = j+1 , l = n-1;
+            while(k<l){
+                int total  = nums[i] + nums[j] + nums[k] + nums[l];
+                if(total > target){
+                    l--;
+                }else if(total < target){
+                    k++;
+                }else{
+                    vector<int> temp = {nums[i],nums[j],nums[k],nums[l]};
+                    ans.push_back(temp);
+                    k++;
+                    
+                    while( k<l && nums[k] == nums[k-1]){
+                        k++;
+                    }
+                    
+                }
+            }
+        }
+    }
+    return ans;
+}
+void solve(){
+   int n,target;
+   cin>>n>>target;
+   vector<int> nums(n);
+   for(auto &it : nums){
+        cin>>it;
+   }
+   vector<vector<int>> res = fourSum(nums,target);
+   for(auto i : res){
+        for(auto j : i){
+            cout<<j<<" ";  
+        }
+        cout<<endl;
+   }
+   
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    auto start = chrono::high_resolution_clock::now();
+
+    int t;
+    cin >> t;
+    
+    while (t--) {
+        solve();
+    }
+
+    auto end = chrono::high_resolution_clock::now();
+    auto duration = chrono::duration_cast<chrono::milliseconds>(end - start);
+    cerr << "Execution Time: " << duration.count() << " ms" << std::endl;
+
+    return 0;
+}
