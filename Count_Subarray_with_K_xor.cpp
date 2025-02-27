1bfa1c62dfc6e68e9dc8cbcaea4630ad1aa1adaa
AdityaGupta28 <codewithadi28@gmail.com>
2025-02-28 00:23:37 +0530

    Same as subarray with k sum

---
+++ Count_Subarray_with_K_xor.cpp
@@ -0,0 +1,85 @@
+/* 
+    author: Aditya Gupta 
+    created: 28.02.2025 00:00:02
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifndef ONLINE_JUDGE
+// Template to debug 1 variable
+template<typename T>
+void debug1(const char* varName, T varValue) {
+    cout << varName << " = " << varValue << endl;
+}
+// Template to debug 2 variables
+template<typename T1, typename T2>
+void debug2(const char* varName1, T1 varValue1, const char* varName2, T2 varValue2) {
+    cout << varName1 << " = " << varValue1 << " " << varName2 << " = " << varValue2 << endl;
+}
+// Template to print a vector
+template<typename T>
+void debug_vector(const char* varName, const vector<T>& vec) {
+    cout << varName << " = [";
+    for (size_t i = 0; i < vec.size(); ++i) {
+        cout << vec[i];
+        if (i < vec.size() - 1) cout << ", ";
+    }
+    cout << "]" << endl;
+}
+
+#define debug1(var) debug1(#var, var)
+#define debug2(var1, var2) debug2(#var1, var1, #var2, var2)
+#define debug_vector(vec) debug_vector(#vec, vec)
+
+#else
+#define debug1(var)
+#define debug2(var1, var2)
+#define debug_vector(vec)
+#endif
+
+#define int long long
+int subarrayXOR(vector<int> &arr , int k){
+    int n = arr.size();
+    map<int,int> track;
+    int curr = 0,ans = 0;   
+    for(int i=0;i<n;i++){
+        curr ^= arr[i];
+        if(curr == k){
+            ans++;
+        }
+        if(track.find(curr^k) != track.end()){
+            ans+=track[curr ^ k];;
+        }
+        track[curr]++;
+    }
+    return ans;
+}
+void solve(){
+   int n,k;
+   cin>>n>>k;
+   vector<int> arr(n);
+   for(auto &it : arr){
+        cin>>it;
+   }
+
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    auto start = chrono::high_resolution_clock::now();
+
+    int t;
+    cin >> t;
+    
+    while (t--) {
+        solve();
+    }
+
+    auto end = chrono::high_resolution_clock::now();
+    auto duration = chrono::duration_cast<chrono::milliseconds>(end - start);
+    cerr << "Execution Time: " << duration.count() << " ms" << std::endl;
+
+    return 0;
+}
